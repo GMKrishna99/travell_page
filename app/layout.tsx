313f9d9c0a50444ce083830cfe6c557c6f736c84
GMKrishna99 <gmohankrishna23@gmail.com>
2023-10-17 09:52:54 +0530

    initial setup for build

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import './globals.css'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+
+const nunito = Nunito({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Travel Guide',
+  description: 'Travel partner with the facilities of Campaign and Trekking',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={nunito.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-teal-800'>hello world</h1>
+    </>
+  )
+}

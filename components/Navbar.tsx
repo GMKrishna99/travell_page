0967bc8a6b3e9d86fa9e3ce1f274387972e27be6
GMKrishna99 <gmohankrishna23@gmail.com>
2023-10-17 10:11:42 +0530

    chore : navbar logo added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
+import Link from "next/link"
+import SiteLogo from '../public/images/hilink-logo.svg'
+import Image from "next/image"
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className=" flexBetween max-container padding-container relative z-30 py-5">
+            <Link href="/">
+                <Image src={SiteLogo} alt="logo" width={74} height={29} />
+            </Link>
+        </nav>
     )
 }
 

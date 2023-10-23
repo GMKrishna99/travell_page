e63fca5a27fd8a79ceff5eae4b6f2c6afab1dea0
GMKrishna99 <gmohankrishna23@gmail.com>
2023-10-23 11:35:50 +0530

    chore : hover color added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
             </Link>
             <ul className="hidden h-full gap-12 lg:flex">
                 {NAV_LINKS.map((link) => (
-                    <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">{link.label}</Link>
+                    <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold hover:text-[#30AF5B] ">{link.label}</Link>
                 ))}
             </ul>
             <div className="lg:flexCenter hidden">
